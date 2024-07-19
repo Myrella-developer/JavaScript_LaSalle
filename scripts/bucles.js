246858cdec36b93a489fe7b99a9bc5a8cf359aa2
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-07-19 20:39:50 +0200

    ejercicios de bucle

--- scripts/bucles.js
+++ scripts/bucles.js
@@ -64,12 +64,81 @@ alumnos.forEach(alumno => {
 
 // TODO: escribe un bucle que muestre 10 veces un mensaje por la consola con el text en color rojo y azul alternativamente
 
+for (i = 0; i< 5; i++){
+    console.log('%cHola mundo', 'color: red');
+
+    for (let y = 0; y < 5; y++){
+        console.log('%cHola mundo', 'color: blue');
+
+    }
+    
+}
+
+
 // TODO: escribe un bucle que muestre los números del 0 al 20 y muestre a su lado "hola" si es multiple de 2, "adeu" si es multiple de 3 i "què tal?" si es múltiple de 5
 
-// TODO: fes un contador que només mostri els números que tinguin un dígit contingut a la string definida per l'usuari (amb prompt) fins a 100
+for (let i = 0; i <= 20; i++) { 
+   if(i % 2 == 0){
+        console.log("Hola " + i);
+   } else if (i % 3 == 0){
+        console.log("Adeus" + i);
+   }else if(i % 5 == 0){
+        console.log("Qué tal?");
+   }
+}
 
-// TODO: llista de la compra. Fes que un prompt es repeteixi fins que l'usuari introdueixi una paraula clau (useu BREAK); fins llavors, cada paraula introduida serà un item de la llista de la compra, que s'escriurà després de que l'usuari l'aturi amb el format:       
-//                       Llista de la compra:
-//                       - Pa
-//                       - Mantega
-//                       - Aigua
+// TODO: haz un contador que muestre en la consola los números del 1 al número que introduzcas como parámetro (hasta 1000 como máximo)
+
+let numero = prompt("Dime un numero entre el 1 al 1000");
+
+for(let i = 1; i <=numero; i++){
+    console.log(i);
+}
+
+// TODO: haz un contador que solo muestre los números que contengan un dígito que esté contenido en la cadena definida por el usuario (usando prompt) hasta 100
+
+let digitos = prompt("Introduce una cadena de digitos (0-9): ");
+
+if(/\d+$/.test(digitos)){
+    for(let i = 0; i<= 100; i++){
+        let digitoStr = i.toString();
+    
+        for (let j = 0; j < digitos.length; j++){
+            if(digitoStr.includes(digitos[j])){
+                console.log(i);
+                break;
+            }
+        }
+    }
+} else{
+    console.log("Introduzca un digito valido");
+}
+// TODO: lista de la compra. Haz que un prompt se repita hasta que el usuario introduzca una palabra clave (usa BREAK); hasta entonces, cada palabra introducida será un ítem de la lista de la compra, que se mostrará después de que el usuario la detenga con el formato:
+//                       Lista de la compra:
+//                       - Pan
+//                       - Mantequilla
+//                       - Agua
+
+
+let listaCompra = [];
+let palabraClave = "stop";
+
+while (true) {
+
+    let item = prompt("Introduce un ítem para la lista de la compra (o 'STOP' para terminar):").toLowerCase();
+
+    if (item === palabraClave) {
+        break;  
+    }
+
+    if (item) {
+        listaCompra.push(item);
+    } else {
+        console.log("No has introducido ningún ítem. Inténtalo de nuevo.");
+    }
+}
+
+console.log("Lista de la compra:");
+listaCompra.forEach(item => {
+    console.log(`- ${item}`);
+});
